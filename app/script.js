cc225dc3c2833b9aabc913224e4f4b874f2d6eb7
Luka Đorđević <lukadordevi@gmail.com>
2024-08-14 23:54:37 +0200

    updated

--- app/script.js
+++ app/script.js
@@ -2,60 +2,17 @@ navigator.serviceWorker && navigator.serviceWorker.register('./../sw.js').then(f
   console.log('Excellent, registered with scope: ', registration.scope);
 });
 
-let display = document.getElementById("display");
-var errSetting = 0;
-let counter = 0;
+window.onload = function () {
+  document.querySelector("#status").innerHTML = 'Loading...';
 
-function errorSetting() {
-  counter++;
-  if (counter == 5) {
-    errSetting = 1;
+  var newDiv = "";
+  
+  const frame = document.querySelector(".frame");
+  for (let i = 0; i < 12; i++) {
+    newDiv = document.createElement("div");
+    newDiv.classList.add("div");
+    frame.appendChild(newDiv);
   }
-}
 
-setInterval(counter = 0, 5000);
-
-Window.onload = function size() {
-  window.resizeTo(700, window.screen.availHeight / 2);
-}
-
-function appendDisplay(char) {
-  if (char == "Del") {
-    display.textContent = display.textContent.slice(0, -1);
-  } else {
-    if (display.textContent === "0") {
-      display.textContent = char;
-    } else {
-      display.textContent += char;
-    }
-  }
-}
-
-/*  Window.onload = function service() {
-  if ("serviceWorker" in navigator) {
-    window.addEventListener("load", function () {
-      navigator.serviceWorker
-        .register("/sw.js")
-        .then(res => console.log("service worker registered"))
-        .catch(err => console.log("service worker not registered", err))
-    })
-  }
-} */
-
-function clearDisplay() {
-  display.textContent = "0";
-}
-
-function evaluateDisplay() {
-  try {
-    //let result = eval(display.textContent);
-    let result = new Function("return " + display.textContent);
-    display.textContent = result();
-  } catch (error) {
-    if (errSetting == 0) {
-      display.textContent = "Error";
-    } else {
-      display.textContent = error;
-    }
-  }
+  document.querySelector("#status").innerHTML = '';
 }
